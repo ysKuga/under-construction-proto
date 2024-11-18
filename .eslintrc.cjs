2ea026d40006a109ec91394c76f0209e58884e80
Yoshihiko Kuga <horrormovies.noga@gmail.com>
2024-11-18 22:21:45 +0900

    #8 chore: .d.ts 用 ESLint 設定追加

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,12 @@ module.exports = {
   extends: ['eslint:recommended', 'next/core-web-vitals'],
   overrides: [
     {
+      files: ['**/*.d.ts'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': ['allow'],
+      }
+    },
+    {
       files: ['**/*.ts', '**/*.tsx'],
       parser: '@typescript-eslint/parser',
       settings: {
