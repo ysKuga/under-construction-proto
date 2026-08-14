6cd6338c14a97c0fabeab228f82f6dda3c552b3a
ysKuga <horrormovies.noga@gmail.com>
2026-08-15 00:11:52 +0900

    #50 feat: useTimeControl03EventListener に allowMultiple オプションを追加
    - 第三引数 options で base hook (useEventListener) の allowMultiple を透過

--- src/prototypes/time-control/time-control-03/_events/_hooks/use-time-control-03-event-listener.ts
+++ src/prototypes/time-control/time-control-03/_events/_hooks/use-time-control-03-event-listener.ts
@@ -8,18 +8,19 @@ import { TimeControl03EventMap } from '../index.types'
  *
  * @param type イベント名
  * @param handler イベント発火時に呼ぶ処理
+ * @param options 多重登録許可の指定
  */
 export const useTimeControl03EventListener = <
   K extends keyof TimeControl03EventMap,
 >(
   type: K,
   handler: (event: CustomEvent<TimeControl03EventMap[K]>) => void,
+  options: { allowMultiple?: boolean } = {},
 ) => {
   const eventTarget = useScopeEventTarget()
 
-  useEventListener<CustomEvent<TimeControl03EventMap[K]>>(
-    type,
-    handler,
-    eventTarget,
-  )
+  useEventListener<CustomEvent<TimeControl03EventMap[K]>>(type, handler, {
+    allowMultiple: options.allowMultiple,
+    target: eventTarget,
+  })
 }
