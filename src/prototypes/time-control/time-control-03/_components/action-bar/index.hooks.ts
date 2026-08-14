7c565fa93beb552104437509962b489096e78607
ysKuga <horrormovies.noga@gmail.com>
2026-08-14 22:15:16 +0900

    #50 refactor: useDispatchDecision を廃止し useActionBar で dispatcher を直接呼ぶ
    1 行のオブジェクトアクセスをラップするだけになり、部品 hook として切り出す価値が薄れたため。resetAll (#50 19129e5) と同じ理由による意図的な例外。

--- src/prototypes/time-control/time-control-03/_components/action-bar/index.hooks.ts
+++ src/prototypes/time-control/time-control-03/_components/action-bar/index.hooks.ts
@@ -1,6 +1,5 @@
 import { useTimeControl03EventDispatcher } from '../../_events'
 
-import { useDispatchDecision } from './_hooks/use-dispatch-decision'
 import { useProgressMode } from './_hooks/use-progress-mode'
 import { ActionBarProps, UseActionBarReturn } from './index.types'
 
@@ -10,9 +9,14 @@ import { ActionBarProps, UseActionBarReturn } from './index.types'
  * @param props ActionBar に渡される props
  */
 export const useActionBar = (props: ActionBarProps): UseActionBarReturn => {
+  const { actorIds } = props
+
   const { progressMode, toggleProgressMode } = useProgressMode(props)
-  const { dispatchDecision } = useDispatchDecision(props)
   const timeControl03EventDispatcher = useTimeControl03EventDispatcher()
+
+  const dispatchDecision = () => {
+    timeControl03EventDispatcher['dispatch-decision']({ actorIds })
+  }
 
   const resetAll = () => {
     timeControl03EventDispatcher['reset-all']()
