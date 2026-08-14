05760e31658460aa58b869232bb451b202590120
ysKuga <horrormovies.noga@gmail.com>
2026-08-15 01:51:32 +0900

    #50 feat: useTimeControl03EventPending を追加
    scope 専用 EventTarget 向けに useEventPending をラップ、集約 index から
    re-export

---
+++ src/prototypes/time-control/time-control-03/_events/_hooks/use-time-control-03-event-pending.ts
@@ -0,0 +1,15 @@
+import { useEventPending } from '@/hooks/use-event-listener'
+
+import { useScopeEventTarget } from '../_contexts/scope-event-context'
+import { TimeControl03EventMap } from '../index.types'
+
+/**
+ * scope (この time-control-03 instance) 専用 EventTarget 上の type が現在 pending かどうかを観測する
+ *
+ * @param type イベント名
+ */
+export const useTimeControl03EventPending = <
+  K extends keyof TimeControl03EventMap,
+>(
+  type: K,
+) => useEventPending(type, { target: useScopeEventTarget() })
