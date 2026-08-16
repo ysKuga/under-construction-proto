7c6dadae749b5716ee5e5e272d5401fa728f05eb
ysKuga <horrormovies.noga@gmail.com>
2026-08-17 00:13:04 +0900

    #50 feat: actor-settings store に生 store 取得 hook を追加
    computed 値の生成 (store.subscribe + getState) には selector 購読でなく
    生 store インスタンスが必要なため、position store と同様の useStoreApi
    export パターンを踏襲する。

--- src/prototypes/time-control/time-control-03/_stores/actor-settings/context.tsx
+++ src/prototypes/time-control/time-control-03/_stores/actor-settings/context.tsx
@@ -2,8 +2,16 @@ import { createStoreContext } from '@/stores/utils/create-store-context'
 
 import { ActorSettingsState } from './types'
 
-const { StoreContext, useStoreSelector } =
+const { StoreContext, useStoreApi, useStoreSelector } =
   createStoreContext<ActorSettingsState>('ActorSettings')
 
 export const ActorSettingsStoreContext = StoreContext
 export const useActorSettingsStore = useStoreSelector
+
+/**
+ * 生の store を返す
+ *
+ * - `_computed` 等、他 store から算出する派生値の store 生成 (`store.subscribe` +\
+ *   `store.getState()`) 向け
+ */
+export const useActorSettingsStoreApi = useStoreApi
