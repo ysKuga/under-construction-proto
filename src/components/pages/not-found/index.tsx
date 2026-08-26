1ac6bab2160120afbadb61024b927ecbe3036876
ysKuga <horrormovies.noga@gmail.com>
2026-08-26 09:32:25 +0900

    #96 fix: Link の適用範囲変更

--- src/components/pages/not-found/index.tsx
+++ src/components/pages/not-found/index.tsx
@@ -11,20 +11,25 @@ const NotFound = () => {
       <h1 className="text-3xl font-extrabold tracking-tight text-gray-900">
         Not Found
       </h1>
-      <p className="text-[12rem] font-black leading-none text-gray-300">404</p>
-      <Link href={paths.home.getHref()}>
-        <BoxBot
-          autoRotate={false}
-          clickActionMap={{ body: ACTION_SPIN, head: ACTION_SPIN }}
-          hopping
-          lightPosition={[0, 1.5, 6]}
-          mode="3d"
-          orbit={false}
-          rotationY={Math.PI}
-          shadowVariant="cast"
-          style={{ height: 160, width: 160 }}
-        />
-      </Link>
+      <p>
+        <Link
+          className="text-[12rem] font-black leading-none text-gray-300 hover:text-gray-400"
+          href={paths.home.getHref()}
+        >
+          404
+        </Link>
+      </p>
+      <BoxBot
+        autoRotate={false}
+        clickActionMap={{ body: ACTION_SPIN, head: ACTION_SPIN }}
+        hopping
+        lightPosition={[0, 1.5, 6]}
+        mode="3d"
+        orbit={false}
+        rotationY={Math.PI}
+        shadowVariant="cast"
+        style={{ height: 160, width: 160 }}
+      />
     </div>
   )
 }
